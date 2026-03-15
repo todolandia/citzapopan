a53ef024e77c884ef010536d29626e2955de23ca
Citzapopan <citzapopan@gmail.com>
2026-03-15 11:02:53 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", function(event) {
+console.log("Service Worker instalado");
+});
+
+self.addEventListener("fetch", function(event) {
+event.respondWith(fetch(event.request));
+});
